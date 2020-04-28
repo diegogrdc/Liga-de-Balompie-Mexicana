4f14acc75d277f61b61c4f61044cde3bb98b5a05
Diego García <diegomysterio@gmail.com>
2020-04-28 15:23:43 -0500

    Funcionalidad Boton Jugar
    Agregar Funcionalidad Boton Jugar

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update() {
+        if (Input.GetMouseButton(0))
+            SceneManager.LoadScene("LaberintoConJugador");
+    }
+
+}
