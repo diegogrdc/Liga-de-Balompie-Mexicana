5c99494ab799c791d1ca5a441a93f4fa89f9efe0
Ernesto1608 <A01365919@itesm.mx>
2020-04-28 16:32:53 -0500

    PlayButton

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -5,15 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    void Update() {
-        if (Input.GetMouseButton(0))
+    public void PlayButton(){
             SceneManager.LoadScene("LaberintoConJugador");
-    }
-
+        }
 }
