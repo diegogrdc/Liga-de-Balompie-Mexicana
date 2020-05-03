9efd216670f0226c1bc08040a8f7f894c9d98c84
Diego García <diegomysterio@gmail.com>
2020-05-03 15:32:18 -0500

    Agregue Puertas/Llaves
    Agregue Puertas/Llaves de colores.
    El jugador puede tomar las llaves, y esto le permitira abrir puertas del mismo color y avanzar en el juego

---
+++ Assets/Scripts/ScriptsKeyDoor/KeyDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDoor : MonoBehaviour
+{
+    [SerializeField] private Key.KeyType keyType;
+
+    public Key.KeyType GetKeyType() {
+    	return keyType;
+    }
+
+    public void OpenDoor() {
+    	gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ScriptsKeyDoor/KeyHolder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyHolder : MonoBehaviour
+{
+    private List<Key.KeyType> keyList;
+
+    private void Awake() {
+    	keyList = new List<Key.KeyType>();
+    }
+
+    public void AddKey(Key.KeyType keyType) {
+    	Debug.Log("Added Key " + keyType );
+    	keyList.Add(keyType);
+    } 
+
+    public void RemoveKey(Key.KeyType keyType) {
+    	keyList.Remove(keyType);
+    }
+
+    public bool ContainsKey(Key.KeyType keyType) {
+    	return keyList.Contains(keyType);
+    }
+
+    private void OnTriggerEnter2D( Collider2D collider ) {
+    	Key key = collider.GetComponent<Key>();
+    	if( key != null ) {
+    		AddKey(key.GetKeyType());
+    		Destroy(key.gameObject);
+    	}
+
+    	KeyDoor keyDoor = collider.GetComponent<KeyDoor>();
+    	if( keyDoor != null ) {
+    		if( ContainsKey(keyDoor.GetKeyType()) ) {
+    			keyDoor.OpenDoor(); 
+    			RemoveKey(keyDoor.GetKeyType());
+    		}
+    	}
+    }
+}
