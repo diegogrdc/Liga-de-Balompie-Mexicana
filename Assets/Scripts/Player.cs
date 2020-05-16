1a74b431f669c1b4aeb9c1bc234c3d1721952d16
Mariano Shaar <>
2020-05-16 17:48:43 -0500

    primeros archivos para la vida

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public class PlayerStats {
+        public float Health = 100f;
+    }
+
+    public PlayerStats playerStats = new PlayerStats();
+
+    public void DamagePlayer (int damage) {
+        playerStats.Health -= damage;
+        if (playerStats.Health <= 0) Destroy (this);
+    }
+    
+
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -41,6 +41,10 @@ public class PlayerMovement : MonoBehaviour
                 {
                      other.gameObject.SetActive(false);
                 }
+        if (other.gameObject.CompareTag("Zombie"))
+                {
+                    Destroy (this);
+                }
     }
 
 }
