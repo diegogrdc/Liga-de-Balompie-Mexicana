7ac7f06acfd373a0bb19da25ae59058e7760b1be
Diego García <diegomysterio@gmail.com>
2020-06-08 13:00:23 -0500

    Added Pause Menu
    Added Pause Menu

--- Assets/Scripts/Pausa.cs
+++ Assets/Scripts/Pausa.cs
@@ -7,27 +7,9 @@ public class Pausa : MonoBehaviour
 {
 	void Update( )
     {
-        if( Input.GetKeyDown( KeyCode.P ) ){
-            if(Time.timeScale == 1)
-        	{
-            	Time.timeScale = 0;
-        	}
-        	else
-        	{
-        	    Time.timeScale = 1;
-        	}
-        }
     }
 
     public void Pause (){
-	if(Time.timeScale == 1)
-        {
-            Time.timeScale = 0;
-        }
-        else
-        {
-            Time.timeScale = 1;
-        }
 }
 
 }
