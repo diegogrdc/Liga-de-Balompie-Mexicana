5efcbc1840d80bcd9d330e31830541aad2e2ae92
Adame <ernesto.adame.cendejas@gmail.com>
2020-06-10 22:06:50 -0500

    camara

--- Assets/Scripts/ScriptsAdame/SigueCamara.cs
+++ Assets/Scripts/ScriptsAdame/SigueCamara.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class SigueCamara : MonoBehaviour
 {
-    private Transform playerTransform;
+
+    private Transform playerTransform;   
     // Start is called before the first frame update
     void Start()
     {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").Transform;
-
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 temp = transform.position;
+        temp.x = playerTransform.position.x;
+        temp.y = playerTransform.position.y;
+
+        transform.position = temp; 
+
     }
+
 }
