6c2c71834df077664d86403db2682bca019f1b81
Ernesto1608 <A01365919@itesm.mx>
2020-06-12 21:42:03 -0500

    Nivel2Completo

---
+++ Assets/Scripts/ZombiesScripts/Zombie1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie1 : MonoBehaviour
+{
+
+    static public float speed = 0.02f,last;
+    
+    public Animator anim;
+
+    public Rigidbody2D rb;
+
+    void Update() {
+        if(Time.timeScale == 0)
+        speed = 0;
+
+        if( transform.position.x < -17.0 && Time.timeScale == 1) {
+            speed = 0.02f;
+            last = speed;
+            anim.SetFloat("Horizontal",1);
+        }
+        if( transform.position.x > 12.0 && Time.timeScale == 1) {
+            speed = -0.02f;
+            last = speed;
+            anim.SetFloat("Horizontal",-1);
+        }
+
+        if(transform.position.x > -17.0 && transform.position.x < 12.0 && Time.timeScale == 1){
+            if(last > 0){
+                anim.SetFloat("Horizontal",1);
+            }
+            if(last < 0){
+                anim.SetFloat("Horizontal",-1);
+            }
+            speed = last;
+        }
+
+        transform.position += new Vector3( speed , 0.000f , 0.0000f );
+    }
+}
+
+
+
---
+++ Assets/Scripts/ZombiesScripts/Zombie3.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie3 : MonoBehaviour
+{
+
+    static public float speed = 0.02f,last;
+    
+    public Animator anim;
+
+    public Rigidbody2D rb;
+
+    void Update() {
+        if(Time.timeScale == 0)
+        speed = 0;
+
+        if( transform.position.x < -23.0 && Time.timeScale == 1) {
+            speed = 0.02f;
+            last = speed;
+            anim.SetFloat("Horizontal",1);
+        }
+        if( transform.position.x > -9.0 && Time.timeScale == 1) {
+            speed = -0.02f;
+            last = speed;
+            anim.SetFloat("Horizontal",-1);
+        }
+
+        if(transform.position.x > -23.0 && transform.position.x < -9.0 && Time.timeScale == 1){
+            if(last > 0){
+                anim.SetFloat("Horizontal",1);
+            }
+            if(last < 0){
+                anim.SetFloat("Horizontal",-1);
+            }
+            speed = last;
+        }
+
+        transform.position += new Vector3( speed , 0.000f , 0.0000f );
+    }
+}
+
+
